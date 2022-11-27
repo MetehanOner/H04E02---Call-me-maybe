1e1391fa79c26f442ca2c7db073f3faeb71d692c
MetehanOner <a.metehanoner@gmail.com>
2022-11-27 16:25:16 +0100

    Erste Commit

--- src/de/tum/in/ase/Phone.java
+++ src/de/tum/in/ase/Phone.java
@@ -6,4 +6,59 @@ public class Phone {
     //TODO: Implement getter and setter methods for the attributes id, brand, price, touchscreen
     //TODO: Implement the toString method for the Phone class
 
+    public static int nextPhoneId = -1;
+    private final int id;
+    private String brand;
+    private double price;
+    private boolean touchscreen;
+
+    public Phone(String brand, double price, boolean touchscreen){
+        setBrand(brand);
+        setPrice(price);
+        setTouchscreen(touchscreen);
+
+        id = nextPhoneId+1;
+        nextPhoneId = nextPhoneId+1;
+    }
+
+    public void setNextPhoneId(int nextPhoneId) {
+        Phone.nextPhoneId = nextPhoneId;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setTouchscreen(boolean touchscreen) {
+        this.touchscreen = touchscreen;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getNextPhoneId() {
+        return nextPhoneId;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public boolean isTouchscreen() {
+        return touchscreen;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
